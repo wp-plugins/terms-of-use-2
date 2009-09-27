d31e67c282cb6f2f1fc2c6f0f2e8f12f63bf493a
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-27 17:04:55 +0000

    changed base folder name from terms-of-use to terms-of-use-2
    git-svn-id: https://plugins.svn.wordpress.org/terms-of-use-2/tags/1.1@158428 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tou-config.php
@@ -0,0 +1,15 @@
+<?php
+define('TOU_PLUGIN_TITLE','Terms of Use');
+define('TOU_PLUGIN_NAME','terms-of-use-2');
+define('TOU_PATH',WP_PLUGIN_DIR.'/'.TOU_PLUGIN_NAME);
+define('TOU_URL',WP_PLUGIN_URL.'/'.TOU_PLUGIN_NAME);
+
+global $wpmu_version;
+
+// Check for WPMU installation
+if($wpmu_version)
+    define('IS_WPMU', 1);
+else
+    define('IS_WPMU', 0);
+
+?>
