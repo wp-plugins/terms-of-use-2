d74d57f91a56213bdc889a86de6b719e3dca37bf
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-07 20:06:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/terms-of-use-2/tags/1.9@161358 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- terms-of-use.php
+++ terms-of-use.php
@@ -49,7 +49,7 @@ add_action ( 'admin_init', 'tou_tinymce' );
 //check is the user has agreed to the terms and conditions
 function tou_check(){
     global $user_ID, $user_level, $tou_settings;
-    if (get_usermeta($user_ID, 'terms_and_conditions' || $_GET['page'] == TOU_PLUGIN_NAME. '/terms-and-conditions.php')) return;
+    if ($user_level == 10 || get_usermeta($user_ID, 'terms_and_conditions' || $_GET['page'] == TOU_PLUGIN_NAME. '/terms-and-conditions.php')) return;
    
     if ($_SERVER["REQUEST_URI"] and $tou_settings['admin_page'])
         $current_page = strpos($_SERVER["REQUEST_URI"], $tou_settings['admin_page']);
